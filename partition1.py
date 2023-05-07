fa9a3a0201ddef1e088f63eb04f0c5d97ad17218
Vishnu Kulkarni <vishnukulkarni13@gmail.com>
2023-05-07 12:38:11 -0700

    Display details working

--- partition1.py
+++ partition1.py
@@ -1,5 +1,11 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QDialog, QScrollArea
 from qtpy.QtWidgets import QVBoxLayout, QLabel, QFrame, QPushButton, QTextEdit, QCheckBox, QLineEdit, QComboBox
+import common
+from partition2 import Partition2
+from partition3 import Partition3
+from partition4 import Partition4
+
 
 class Partition1(QVBoxLayout):
     CONNECTED = False
@@ -8,7 +14,8 @@ class Partition1(QVBoxLayout):
         super().__init__()
 
         # Create label for partition
-        self.label = QLabel('Dashboard')
+        self.label = QLabel('DASHBOARD')
+        self.label.setFont(QFont("Arial", 8, QFont.Bold))
         self.label.setFixedSize(200, 15)
         self.frame = QFrame()
         self.frame.setFrameShape(QFrame.Box)
@@ -23,6 +30,7 @@ class Partition1(QVBoxLayout):
         self.dropdown_label = QLabel('List of BiSi')
         self.dropdown = QComboBox()
         self.dropdown.addItems(self.GetBisiList())
+
         self.selected_option = self.dropdown.currentText()
         self.dropdown.activated.connect(self.GetNameOfBisiSelected)
 
@@ -38,10 +46,19 @@ class Partition1(QVBoxLayout):
         self.dropdown_ppl = QComboBox()
         #self.dropdown_ppl.addItems(self.GetPplList()) #this shud fetch data from db based on BiSI name selected
         self.selected_option_ppl = self.dropdown_ppl.currentText()
-        self.dropdown_ppl.activated.connect(self.GetNameOfPersonSelected)
-
+        self.dropdown_ppl.activated.connect(self.DisplayPersonDetails)
 
         self.textdisplay = QTextEdit()
+
+        self.scroll = QScrollArea()
+        self.textdisplay = QTextEdit()
+        self.textdisplay.setReadOnly(True)  # make the display area read-only
+        self.scroll.setWidget(self.textdisplay)
+        self.scroll.setWidgetResizable(True)
+
+        # Add clear button for text display area
+        self.clear_button = QPushButton('Clear')
+        self.clear_button.clicked.connect(self.ClearTextDisplay)
 
         # Add button, checkbox, text input, dropdown menu, and text display area to partition layout
         self.addWidget(self.label)
@@ -57,49 +74,67 @@ class Partition1(QVBoxLayout):
 
         self.addWidget(self.line2)
 
-
         self.addWidget(self.dropdown_label_ppl)
         self.addWidget(self.dropdown_ppl)
 
-        self.addWidget(self.textdisplay)
+        self.addWidget(self.scroll)
+
+        self.addWidget(self.clear_button)
+
         self.addWidget(self.frame)
 
 
     def Connect(self):
         #SAL : add connection code here. After success set 'Partition1.CONNECTED = True'
-
-        Partition1.CONNECTED = True
+        common.CONNECTED = True
+        #Partition1.CONNECTED = True
         print("connected")
 
         #if connection fails,set 'Partition1.CONNECTED = False'
 
-        # Clear the current items in the drop-down menu
+        # Clear the current items in the bisi drop-down menu
         self.dropdown.clear()
         self.dropdown.addItems(self.GetBisiList())
 
     def GetBisiList(self):
-        if Partition1.CONNECTED:
-            bisilist = ["aa","bb","cc"] # SAL : call the 'ListOfBisi' api here
+        if common.CONNECTED:
+            bisilist = ["bisi 1","bisi 2","bisi 33"] # SAL : call the 'ListOfBisi' api here
         else:
             bisilist = ['Not Connected'] #VK : default list when not connected to db. Think over it
         return bisilist
 
     def GetNameOfBisiSelected(self):
-        text = self.dropdown.currentText()
-        print(text)
+        bisiName = self.dropdown.currentText()
+
+        # Set the text display area's text to the desired text
+        self.textdisplay.append(f"Details of : {bisiName} BC")
+
         self.GetPplList()
-        return text
+        #return text
 
     def GetPplList(self):
-        if Partition1.CONNECTED:
-            ppllist = ["pp1","pp2","pp3"] # SAL : call the 'ListOfPll' api here for specific bisi
+        if common.CONNECTED:
+            pplList = ["pp1", "pp2", "pp3"]  # SAL : call the 'ListOfPll' api here for specific bisi
         else:
-            ppllist = ['Not Connected'] #VK : default list when not connected to db. Think over it
-        # Clear the current items in the drop-down menu
+            pplList = ['Not Connected']  # VK : default list when not connected to db. Think over it
+
+        # Clear the current items in the ppl list  drop-down menu
         self.dropdown_ppl.clear()
-        self.dropdown_ppl.addItems(ppllist)
+        self.dropdown_ppl.addItems(pplList)
 
     def GetNameOfPersonSelected(self):
         personName = self.dropdown_ppl.currentText()
         print(personName)
         return personName
+
+    def DisplayPersonDetails(self):
+        personName = self.GetNameOfPersonSelected()
+        # Set the text display area's text to the desired text
+        self.textdisplay.append(f"Person Selected : {personName}")
+
+        #partition2_instance.displayPersonDetailsFull(self)
+
+    def ClearTextDisplay(self):
+        self.textdisplay.clear()
+
+
--- partition2.py
+++ partition2.py
@@ -1,11 +1,16 @@
 from qtpy.QtWidgets import QVBoxLayout, QLabel, QFrame, QPushButton, QTextEdit, QCheckBox, QLineEdit, QComboBox
 
+import common
+from partition3 import Partition3
+from partition4 import Partition4
+
 class Partition2(QVBoxLayout):
+
     def __init__(self):
         super().__init__()
 
         # Create label for partition
-        self.label = QLabel('Partition 2')
+        self.label = QLabel('PERSON DETAILS')
         self.label.setFixedSize(200, 15)
         self.frame = QFrame()
         self.frame.setFrameShape(QFrame.Box)
@@ -14,15 +19,29 @@ class Partition2(QVBoxLayout):
         self.button = QPushButton('Button')
         self.checkbox = QCheckBox('Checkbox')
         self.textinput = QLineEdit()
-        self.dropdown = QComboBox()
-        self.dropdown.addItems(['Option 1', 'Option 2', 'Option 3'])
-        self.textdisplay = QTextEdit()
+
+        #self.dropdown = QComboBox()
+        #self.dropdown.addItems(['Option 1', 'Option 2', 'Option 3'])
+
+
+        #self.dropdown_label_ppl = QLabel('List of People')
+        #self.dropdown_ppl = QComboBox()
+        #self.dropdown_ppl.addItems(self.GetPplList()) #this shud fetch data from db based on BiSI name selected
+        #self.selected_option_ppl = self.dropdown_ppl.currentText()
+        #self.dropdown_ppl.activated.connect(self.GetNameOfPersonSelected)
+
+
+        self.textdisplay_partition2 = QTextEdit()
 
         # Add button, checkbox, text input, dropdown menu, and text display area to partition layout
         self.addWidget(self.label)
         self.addWidget(self.button)
         self.addWidget(self.checkbox)
         self.addWidget(self.textinput)
-        self.addWidget(self.dropdown)
-        self.addWidget(self.textdisplay)
+
+        #self.addWidget(self.dropdown_label_ppl)
+        #self.addWidget(self.dropdown_ppl)
+
+        self.addWidget(self.textdisplay_partition2)
         self.addWidget(self.frame)
+
